65b465d9cd5105395e9b9e5e599966fe07087951
Michał Szajbe <michal.szajbe@gmail.com>
2023-05-04 17:39:28 +0200

    Bump version to 1.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EtcdEx.MixProject do
   def project do
     [
       app: :etcdex,
-      version: "1.1.6",
+      version: "1.1.7",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
